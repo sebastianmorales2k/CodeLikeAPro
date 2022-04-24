f9d0f5ca505efc05cf61091e0902cd8f112e58d2
JOSE DIDIER AGUDELO GARCiA <joseagudelo298770@correo.itm.edu.co>
2022-04-24 13:26:13 -0500

    users

--- aplicattion1/Migrations/20220424180847_adduserentities.Designer.cs
+++ aplicattion1/Migrations/20220424180847_adduserentities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using aplicattion1.Data;
 
@@ -11,9 +12,10 @@ using aplicattion1.Data;
 namespace aplicattion1.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220424180847_adduserentities")]
+    partial class adduserentities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
