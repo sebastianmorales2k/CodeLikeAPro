3fcc84e075023bbc9f5ad42c390324b7ac261a6e
sebastianmorales2k <juanmorales298578@correo.itm.edu.co>
2022-04-24 13:27:48 -0500

    migrations

---
+++ aplicattion1/Migrations/20220424182631_allEntities.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aplicattion1.Migrations
+{
+    public partial class allEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_Name_CourseTypeId",
+                table: "Courses");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "DocumentType",
+                newName: "Id_DocumentType");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Name",
+                table: "Courses",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_Name",
+                table: "Courses");
+
+            migrationBuilder.RenameColumn(
+                name: "Id_DocumentType",
+                table: "DocumentType",
+                newName: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Name_CourseTypeId",
+                table: "Courses",
+                columns: new[] { "Name", "CourseTypeId" },
+                unique: true);
+        }
+    }
+}
